696cd2a2b87de57e88101178f618a2d0b4c172df
wsrtka <witold.serwatka@pm.me>
2021-04-15 18:05:02 +0200

    add exception level printing

--- operating_system/src/kernel.c
+++ operating_system/src/kernel.c
@@ -5,7 +5,11 @@ void kernel_main(void)
 {
 	uart_init();
 	init_printf(0, putc);
-	uart_send_string("Hello, world!\r\n");
+	uart_send_string("Kernel is initialized!\r\n");
+
+	// Print current exception level
+	int el = get_el();
+	printf("Exception level: %d \r\n", el);
 
 	while (1) {
 		uart_send(uart_recv());
